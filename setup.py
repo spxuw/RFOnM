c0530bb783838a90dbf7209c97d3ef02388a1479
Xu-Wen Wang <xuwenwang86@gmail.com>
2025-07-22 14:51:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='RFOnM',
+    name='rfonm',
     version='0.1',
-    packages=find_packages(),
+    packages=['rfonm'],
     entry_points={
         'console_scripts': [
-            'RFOnM=RFOnM.main:main',
+            'rfonm=rfonm.main:main',
         ],
     },
     install_requires=[
-        'numpy',
-        'scipy',
-        'networkx',
+    'numpy>=1.26,<2.0',
+    'networkx>=3.4,<4.0',
     ],
     author='Xu-Wen Wang',
     description='RFOnM for disease module detection',
